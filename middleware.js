bb2f5ceb6fcb085b795f136968a834dd7d38f142
DYEDart <dylanlavon@gmail.com>
2021-11-24 16:20:55 -0600

    Finished Implementing login / logout functionality

--- middleware.js
+++ middleware.js
@@ -1,6 +1,8 @@
-module.exports.isLoggedIn = (req, res, next) => {
+module.exports.isLoggedIn = (req, res, next) => {  
     if(!req.isAuthenticated())
     {
+        //Store the URL They are Requesting
+        req.session.returnTo = req.originalUrl
         req.flash('error', 'You must be signed in.')
         return res.redirect('/login')
     }
--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,19 @@ router.get('/register', (req,res)=>{
     res.render('users/register')
 })
 
-router.post('/register', async (req,res)=>{
+router.post('/register', async (req,res, next)=>{
     try{
         const {email, username, password} = req.body
         const user = new User({email, username})
         const registeredUser = await User.register(user, password)
-        req.flash('success', 'Welcome to WritePad!')
-        res.redirect('/writings')
+        req.login(registeredUser, err =>{
+            if(err){
+                return next(err)
+            } else {
+                req.flash('success', 'Welcome to WritePad!')
+                res.redirect('/writings')
+            }
+        })      
     } catch(e) {
         req.flash('error', e.message)
         res.redirect('/register')
@@ -27,6 +33,14 @@ router.get('/login', (req,res)=>{
 
 router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req,res)=>{
     req.flash('success', 'Welcome back to WritePad!')
+    const redirectUrl = req.session.returnTo || '/writings'
+    delete req.session.returnTo
+    res.redirect(redirectUrl)
+})
+
+router.get('/logout', (req,res)=>{
+    req.logout()
+    req.flash('success', 'We hope to see you back soon.')
     res.redirect('/writings')
 })
 
