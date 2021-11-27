9d8b0b68460b1b2c32e13f33161c854d90788533
DYEDart <dylanlavon@gmail.com>
2021-11-27 13:12:43 -0600

    altered dbUrl

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const userRoutes = require("./routes/users");
 
 const MongoDBStore = require("connect-mongo")
 
-const dbUrl = process.env.DB_URL
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/write-pad"
 console.log(dbUrl)
 // mongoose.connect("mongodb://localhost:27017/write-pad");
 mongoose.connect(dbUrl);
