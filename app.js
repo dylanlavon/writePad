7c18ed8d63066c2802b9e610ae9db934e9b08848
DYEDart <dylanlavon@gmail.com>
2021-11-27 13:05:24 -0600

    add start script

--- app.js
+++ app.js
@@ -93,6 +93,8 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
-app.listen(3000, () => {
-    console.log("Serving on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
 });
